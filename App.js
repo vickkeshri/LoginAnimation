256fdd1ec5802794e17b8fcdf2de68c3ad7cfad5
vicky keshri <vicky.k@blickx.com>
2019-09-20 17:55:35 +0530

    Login with Animation

---
+++ App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {Assets} from 'expo-asset';
+import { AppLoading } from 'expo';
+import Music from './components/Music';
+
+
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+}
+
+export default class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      isReady: false
+    }
+  }
+
+  async _loadAssetsAsync() {
+    const imageAssets = cacheImages([
+      require('./images/bg.jpg'),
+    ]);
+
+    await Promise.all([...imageAssets]);
+  }
+
+
+  render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
+    }
+
+    return (
+      <Music/>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
